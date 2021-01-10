2d99d03557b79d722ff4f3d92010c98a207e6e05
Ajaydip Singh <singh@minerva.kgi.edu>
2021-01-10 22:54:49 +0300

    :sparkles: feat: add login layout

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, Password, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
 
     username = StringField(
         'Username', 
-        validators=[DataRequired(), Length(min=5, max=20)],
+        validators=[DataRequired(), Length(min=3, max=20)],
         )
 
     email = StringField(
