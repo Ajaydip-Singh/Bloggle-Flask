6ab1c211a201c8c68ee829390114165d43c2a0bc
Ajaydip Singh <singh@minerva.kgi.edu>
2021-01-10 14:29:48 +0300

    :sparkles: feat: add dummy posts logic

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,32 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Ajaydip Singh',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'Jan 10 2021'
+    },
+    {
+        'author': 'Harmeet Singh',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'Jan 11 2021'
+    }
+]
+
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-    
