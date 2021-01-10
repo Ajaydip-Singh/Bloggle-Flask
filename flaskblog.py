01083b4a188a070e1a29c3aee6cc5f5b48876646
Ajaydip Singh <singh@minerva.kgi.edu>
2021-01-10 15:43:35 +0300

    :sparkles: feat: add  registration form

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
@@ -20,11 +20,11 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('layout.html', posts=posts)
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('layout.html', title=title)
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
