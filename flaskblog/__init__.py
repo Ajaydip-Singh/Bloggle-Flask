51d141789c39da1d9b7aa95cd56341126895154f
Ajaydip Singh <singh@minerva.kgi.edu>
2021-02-01 13:57:05 +0300

    :sparkles: feat: add user authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -7,5 +8,6 @@ app.config['SECRET_KEY'] = '198d4cfd791be36f8397af08d7fa4908'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
