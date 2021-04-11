3caeb3a92a0eea7ac4ed08a76f2988153986e0d4
Ajaydip Singh <singh@minerva.kgi.edu>
2021-04-11 22:16:30 +0300

    :bug: fix: use werkzeug for hashing password

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -81,7 +81,7 @@ def reset_request():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         send_reset_email(user)
-        flash('An email has been with instructions to reset your password', 'info')
+        flash('An email has been sent with instructions to reset your password', 'info')
         return redirect(url_for('users.login'))
     return render_template('reset_request.html', title='Reset Password', form=form)
 
@@ -98,7 +98,7 @@ def reset_token(token):
 
     form = ResetPasswordForm()
     if form.validate_on_submit():
-        user.password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = generate_password_hash(form.password.data)
         db.session.commit()
         flash('Your password has been reset! You can now login', 'success')
         return redirect(url_for('users.login'))
